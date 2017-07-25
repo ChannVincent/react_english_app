1d73b599a1776496fa9106c6ad5e47c973254129
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-07-25 22:52:23 +0200

    EA-1 : init : hello world

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+/*
+entry : first file that webpack is looking for to start your application (root component)
+output : result of webpack compiling all your "resolved files" in 1 uniq file called "bundle.js"
+alias : list of jsx files path to import easily each one of them by just typing the filename
+
+
+*/
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      applicationStyles: 'app/styles/app.scss',
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+}
